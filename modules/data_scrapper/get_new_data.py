43bd0648f81539d0d9083eb68e75c2f8ea52e7cb
João Holz <joao.holz+tegrus@tegrus.com.br>
2021-04-09 11:40:04 -0300

    added function to get today data as dataframe

---
+++ modules/data_scrapper/get_new_data.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+def get_today_data(station = 'A612'):
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('--disable-dev-shm-usage')
+    site = f'https://tempo.inmet.gov.br/TabelaEstacoes/{station}'
+
+    wb = webdriver.Chrome('chromedriver',options = options)
+    wb.get(site)
+
+    html = wb.page_source
+    df = pd.read_html(html)[0]
+    return df
